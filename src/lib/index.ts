ff7c50aa1c4a4d2054c522cc6b5afad20c3d3397
LexRiver <lexriver@gmail.com>
2025-07-13 17:27:59 +0300

    adding types for export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
+import type { ReorderableListProps } from './components/ReorderableList.svelte';
 import ReorderableList from "./components/ReorderableList.svelte";
+import type { FlatTreeNode, ReorderableTreeProps, TreeNode } from './components/ReorderableTree.svelte';
 import ReorderableTree from "./components/ReorderableTree.svelte";
-import type { TreeNode } from './components/ReorderableTree.svelte';
 
-export { ReorderableList, ReorderableTree, type TreeNode };
+export { ReorderableList, ReorderableTree, type FlatTreeNode, type ReorderableListProps, type ReorderableTreeProps, type TreeNode };
 
